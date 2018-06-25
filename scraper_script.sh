50d17d6de4556cc0d9e2fe33ce464387f9fc2983
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-06-25 23:20:56 +0300

    run correct file

--- scraper_script.sh
+++ scraper_script.sh
@@ -4,7 +4,7 @@ years='07 08 09 10 11 12'
 for year in $years
 do
     echo $year
-    python sofifa_scraper.py --year $year
+    python sofifa_extensive_player_scraper.py --year $year
     sleep 10s
 done
 echo "All done"
--- scraper_script_2.sh
+++ scraper_script_2.sh
@@ -4,7 +4,7 @@ years='13 14 15 16 17 18'
 for year in $years
 do
     echo $year
-    python sofifa_scraper.py --year $year
+    python sofifa_extensive_player_scraper.py --year $year
     sleep 10s
 done
 echo "All done"
