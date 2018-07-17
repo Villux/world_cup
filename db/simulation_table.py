6fad05ed21a588ba44e88a8a4b9439194dcc65c9
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-07-17 14:49:07 +0300

    store simulation results

--- db/simulation_table.py
+++ db/simulation_table.py
@@ -1,4 +1,5 @@
-from db.db_interface import execute_statement, fetchall, fetchone
+import pandas as pd
+from db.db_interface import execute_statement, fetchall, fetchone, get_connection
 from db.db_helper import get_value_tuple, build_insert_query
 
 table_name = "match_simulation"
@@ -34,3 +35,8 @@ def get_win_probability(team, match_id):
     total_number_of_matches = get_number_of_simulations_for_match(match_id)
 
     return (home_team_outcomes.get(1, 0) + away_team_outcomes.get(-1, 0))/total_number_of_matches
+
+
+def store_simulation_results(filename):
+    simulations = pd.read_sql_query("""select * from match_simulation;""", get_connection())
+    simulations.to_csv(filename)
