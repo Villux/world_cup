2881f563c8042adc99269c58ff8718a7a73d6b68
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-07-17 14:49:38 +0300

    simulation output filename

--- run_score_model_simulation.py
+++ run_score_model_simulation.py
@@ -1,14 +1,16 @@
 import functools
+import random
+import time
+import socket
 import numpy as np
 import pandas as pd
-import random
-from time import time
 
 from sklearn.ensemble import RandomForestRegressor
 
 from features.data_provider import get_train_test_wc_dataset, get_feature_columns, get_whole_dataset
 from simulation.predictor import ScorePredictor
 from simulation.simulation import run_simulation, get_match_win_probability
+from db.simulation_table import store_simulation_results
 
 
 home = get_whole_dataset("home_score")
@@ -33,9 +35,11 @@ clf.fit(X, y)
 match_template = pd.read_csv('data/original/wc_2018_games.csv')
 predictor = ScorePredictor(clf)
 
-for i in range(0, 10):
+for i in range(0, 1000):
     print(f"Running simulation: {i}")
     run_simulation(match_template, predictor)
 
 teams = pd.unique(match_template[['home_team', 'away_team']].values.ravel('K'))[0:32]
 print(get_match_win_probability(teams, 63))
+
+store_simulation_results(f"data/simulations/{socket.gethostname()}_{round(time.time())}_simulation.csv")
