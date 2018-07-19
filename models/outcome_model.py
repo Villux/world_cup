01f3f17584f2a355349ccaee28258c62c969d239
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-07-19 09:54:03 +0300

    model modules

---
+++ models/outcome_model.py
@@ -0,0 +1,18 @@
+from sklearn.ensemble import RandomForestClassifier
+
+def get_model(params=None, X=None, y=None):
+    if not params:
+        params = {
+            'oob_score' : True,
+            'bootstrap': True,
+            'n_jobs':-1,
+            'n_estimators': 1000,
+            "max_depth": None,
+            "min_samples_leaf": 5,
+            "max_features": "sqrt"
+        }
+    model = RandomForestClassifier(**params)
+    if X and y:
+        model.fit(X, y)
+
+    return model
