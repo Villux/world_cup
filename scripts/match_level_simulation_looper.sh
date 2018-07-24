fcde0d216991e062314abc003a6a03b1409c74ed
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-07-24 09:58:35 +0300

    match level script

---
+++ scripts/match_level_simulation_looper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+do
+    echo "Run match-level simulation loop $i for score prediction"
+    python3 run_score_model_simulation.py --actual
+done
+
+for j in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+do
+    echo "Run match-level simulation loop $j for outcome prediction"
+    python3 run_outcome_model_simulation.py --actual
+done
+
+echo "Finished running simulation"
