403958850415e1fa397f3797ac057e97888523d8
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-08-13 15:26:51 +0300

    optimal params (acc + logloss)

--- models/score_model.py
+++ models/score_model.py
@@ -8,11 +8,12 @@ def get_model(params=None, X=None, y=None, n_estimators=200):
             'n_jobs':-1,
             'n_estimators': n_estimators,
             "max_depth": 8,
-            "min_samples_leaf": 1,
-            "max_features": "sqrt"
+            "min_samples_leaf": 5,
+            "max_features": "log2"
         }
     model = RandomForestRegressor(**params)
     if (X is not None) and (y is not None):
         model.fit(X, y)
 
+    print(params)
     return model
