3259dd07b89dcba71b2f3a97fe81cca8d2d9b599
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-08-14 15:27:43 +0300

    plot cost and positive net flow

--- notebook_helpers.py
+++ notebook_helpers.py
@@ -141,7 +141,7 @@ def plot_bank_and_bets(strategy):
     net_returns = np.array(strategy.get_returns())
     returns = net_returns + 1
     returns[0] *= initial_capital
-    costs = strategy.costs
+    costs = np.array(strategy.costs) * -1
 
     balance_progression = np.cumprod(returns)
     bar_labels = [1 if value > 0 else 0 for value in net_returns]
@@ -153,8 +153,9 @@ def plot_bank_and_bets(strategy):
     colors = {0: 'r', 1: 'g'}
     fig, ax = plt.subplots(figsize=figsize)
     index = np.arange(len(costs))
+    ax.bar(index, winnnings, color='g')
     ax.bar(index, costs, color='r')
-    ax.bar(index, winnnings, color='g', bottom=costs)
+
     ax.set_xticks(np.arange(0, len(net_returns)))
     plt.xticks(rotation='vertical')
 
